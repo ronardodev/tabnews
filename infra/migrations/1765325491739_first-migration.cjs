2d5516b323eb1745f7780e5ea716e4d3ba943ba7
ronardo <ronardo.dev@gmail.com>
2025-12-12 18:08:31 -0300

    altera arquivos de migração para .js (ESM)

--- infra/migrations/1765325491739_first-migration.cjs
+++
@@ -1,8 +0,0 @@
-// export const shorthands = undefined;
-// export const up = (pgm) => {};
-// export const down = (pgm) => {};
-
-exports.shorthands = undefined;
-exports.up = (pgm) => {};
-exports.down = (pgm) => {};
-
---
+++ infra/migrations/1765325491739_first-migration.js
@@ -0,0 +1,8 @@
+export const shorthands = undefined;
+export const up = (pgm) => {};
+export const down = (pgm) => {};
+
+// exports.shorthands = undefined;
+// exports.up = (pgm) => {};
+// exports.down = (pgm) => {};
+
