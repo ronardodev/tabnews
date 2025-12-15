da536b0e8b9c485b377e3335776684924cffeadf
ronardo <ronardo.dev@gmail.com>
2025-12-15 10:50:54 -0300

    altera de ESM para CommonJS

--- infra/migrations/1765325491739_first-migration.js
+++ infra/migrations/1765325491739_first-migration.js
@@ -1,8 +1,8 @@
-export const shorthands = undefined;
-export const up = (pgm) => {};
-export const down = (pgm) => {};
+// export const shorthands = undefined;
+// export const up = (pgm) => {};
+// export const down = (pgm) => {};
 
-// exports.shorthands = undefined;
-// exports.up = (pgm) => {};
-// exports.down = (pgm) => {};
+exports.shorthands = undefined;
+exports.up = (pgm) => {};
+exports.down = (pgm) => {};
 
